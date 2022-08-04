e4b11a6109dfe27c4c5baf1ad74c13aabf927898
unknown <achilihudaniel53@gmail.com>
2022-08-04 12:54:29 +0100

    Second commit

--- exponential.js
+++ exponential.js
@@ -1,16 +1,96 @@
-// Find the Power of a Number using for Loop
+    // Find the Power of a Number using for Loop
 
-let basenum = prompt("Enter Base Number")
-let exponum = prompt("Enter Exponential Number")
+    const calculator = document.querySelector(".cal");
+    const keys = document.querySelector(".line");
 
-function numPower(exp, pow) {
-var num=1;
+    keys.addEventListener("click",function(e){
+        if (e.target.matches("button")){
+            // Do something
+        }
+    });
+    
+    const key =e.target
+    const action =key.dataset.action
 
-for(var i=0;i<pow; i++){
-num=num*exp;
-}
-return num;
-}
 
-alert(numPower(basenum,exponum));
-console.log(numPower(basenum,exponum));
+    if (!action){
+        console.log("number key!")
+    }
+
+
+    if(
+        action ==="add"  ||
+        action ==="subtract" ||
+        action ==="sqrt" ||
+        action ==="multiply" || 
+        action ==="divide"
+    ){
+        alert("operator key!")
+    }
+
+
+    if (action==="decimal"){
+        alert("decimal key!")
+    }
+
+    if (action==="clear"){
+        alert("clear key!")
+    }
+
+    if (action==="calculate"){
+        console.log("equal key!")
+    }
+
+
+
+
+
+    // let equal = document.getElementById("equal");
+
+    // equal.addEventListener(click,equalto)
+    // function equalto(event){
+        
+    // }
+
+
+
+    // inputs.addEventListener("click",function(numPower){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let basenum = prompt("Enter Base Number")
+    // let exponum = prompt("Enter Exponential Number")
+
+    // var inputs = document.getElementById("Exp");
+    // function numPower(exp, pow) {
+    // var num=1;
+
+
+    // for(var i=0;i<pow; i++){
+    // num=num*exp;
+    // }
+    // return num;
+    // }
+    // // })
+
+    // alert(numPower(basenum,exponum));
+    // return numPower(basenum,exponum);
